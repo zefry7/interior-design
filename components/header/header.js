a630b3995b2864dc4b3a95ac3c3c350d5753e16d
nikita petrov <petrov.nikita702@mail.ru>
2023-10-24 11:04:22 +0300

    Потытка №1 перед завершением

--- components/header/header.js
+++ components/header/header.js
@@ -15,3 +15,19 @@ function burgerMenuOpen() {
     burgerContent.classList.toggle("burger__content_active")
     document.body.classList.toggle("body-lock")
 }
+
+let listLink = document.querySelectorAll(".header__item-menu");
+
+listLink.forEach(value => {
+    value.addEventListener("click", (event) => {
+        event.preventDefault()
+        let el = document.querySelector(value.getAttribute("data-scroll"))
+        let y = el.getBoundingClientRect().top + window.scrollY - 140;
+        burgerContent.classList.remove("burger__content_active")
+        document.body.classList.remove("body-lock")
+        window.scrollTo({
+            top: y,
+            behavior: "smooth"
+        })
+    })
+})
---
+++ components/main/spec-section/specSection.js
@@ -0,0 +1,27 @@
+let items = document.querySelector(".spec-section__items")
+let listItem = document.querySelectorAll(".spec-section__item")
+let restartListItem = listItem
+let bo = false
+var stopT
+window.addEventListener("resize", () => {
+    if(window.innerWidth <= 1300 && bo == false) {
+        bo = true
+        listItem.forEach(value => {
+            let a = value.cloneNode(true)
+            items.append(a)
+        })
+
+        stopT = setInterval(() => {
+            items.append(listItem[0])
+            listItem = document.querySelectorAll(".spec-section__item")
+        }, 5000)
+    } else if(window.innerWidth > 1300 && bo == true) {
+        bo = false
+        clearInterval(stopT)
+        items.innerHTML = ""
+        listItem = restartListItem
+        restartListItem.forEach(value => {
+            items.append(value)
+        })
+    }
+})
