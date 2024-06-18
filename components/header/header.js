d333e40efe808b296477e1aa4451016f4ca33586
nikita petrov <petrov.nikita702@mail.ru>
2024-06-19 00:06:12 +0300

    правки в блоке Header

--- components/header/header.js
+++ components/header/header.js
@@ -1,11 +1,17 @@
-let burgerContent = document.getElementsByClassName("burger__content")[0]
+let burgerContent = document.getElementsByClassName("burger__content")[0];
+let listLink = document.querySelectorAll(".header__item-menu");
 
 function burgerMenuOpen() {
     burgerContent.classList.toggle("burger__content_active")
     document.body.classList.toggle("body-lock")
 }
 
-let listLink = document.querySelectorAll(".header__item-menu");
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 768) {
+        burgerContent.classList.remove("burger__content_active")
+        document.body.classList.remove("body-lock")
+    }
+})
 
 listLink.forEach(value => {
     value.addEventListener("click", (event) => {
