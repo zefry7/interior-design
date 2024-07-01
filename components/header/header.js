06c0c6740e6b48e0ad13f45562d2cd4b5dc6a738
nikita petrov <petrov.nikita702@mail.ru>
2024-07-01 12:30:22 +0300

    Добавление функционала для header

--- components/header/header.js
+++ components/header/header.js
@@ -3,12 +3,38 @@ let listLink = document.querySelectorAll(".header__item-menu");
 const burgerButton = document.getElementsByClassName("burger__button")[0];
 
 burgerButton.addEventListener("click", (e) => {
-    e.stopPropagation()
-    burgerContent.classList.toggle("burger__content_active")
-    document.body.classList.toggle("body-lock")
-    e.currentTarget.classList.toggle("burger__button_active")
+    activeMenuBurger(e)
 })
 
+burgerButton.addEventListener("keydown", (e) => {
+    e.stopPropagation()
+    if (e.keyCode == 13) {
+        activeMenuBurger(e)
+    }
+})
+
+function activeMenuBurger(e) {
+    let timeLock = 0
+    let timeVisible = 0
+
+    if (!document.body.classList.contains("body-lock")) {
+        timeLock = 300
+    }
+    if (burgerContent.classList.contains("burger__content_visible")) {
+        timeVisible = 300
+    }
+
+    setTimeout(() => {
+        document.body.classList.toggle("body-lock")
+    }, timeLock)
+
+    setTimeout(() => {
+        burgerContent.classList.toggle("burger__content_visible")
+    }, timeVisible)
+
+    burgerContent.classList.toggle("burger__content_active")
+    e.currentTarget.classList.toggle("burger__button_active")
+}
 
 function clickLogo() {
     window.scrollTo({
@@ -18,7 +44,7 @@ function clickLogo() {
 }
 
 window.addEventListener("resize", () => {
-    if(window.innerWidth > 768) {
+    if (window.innerWidth > 768) {
         burgerContent.classList.remove("burger__content_active")
         document.body.classList.remove("body-lock")
     }
